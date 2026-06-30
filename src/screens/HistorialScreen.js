727958f7c320f8ca4f024a232be8471e09bc61d9
Nicolas <ngramirez313an@gmail.com>
2026-06-30 20:53:49 -0300

    feat: Arreglo de Navegación

--- src/screens/HistorialScreen.js
+++ src/screens/HistorialScreen.js
@@ -10,14 +10,14 @@ import ListaTarea from "../components/ListaTarea";
 
 export default function HistorialScreen() {
   // usa directamente la variable guardada
-  const { pendingTasks } = useContext(TaskContext); // Deja de analizar el array.
+  const { completedTasks } = useContext(TaskContext); // Deja de analizar el array.
 
   return (
     <View>
       <Text>Historial de Completadas</Text>
       <View>
         <ListaTarea
-          data={pendingTasks}
+          data={completedTasks}
           emptyMessage="Aún no has completado ninguna tarea."
         />
       </View>
