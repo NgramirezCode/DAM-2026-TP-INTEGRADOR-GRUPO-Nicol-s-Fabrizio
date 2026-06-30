719e01cd684d212579ec25383e440b36b294d0bb
Nicolas <ngramirez313an@gmail.com>
2026-06-30 16:18:35 -0300

    feat: Agregación del Task y Persistencia

--- src/components/inputTarea.js
+++ src/components/inputTarea.js
@@ -1,17 +1,18 @@
 import React, { useState, useContext } from "react";
 import { View, TextInput, TouchableOpacity, Text } from "react-native";
-import { Controlador } from "../controler/Controlador";
+
+import { TaskContext } from "../context/TaskContext";
 
 // Componente InputTarea que permite al usuario agregar nuevas tareas
 export default function InputTarea() {
   // Estado local para manejar el texto del input
   const [text, setText] = useState("");
-  // Accede a la función addTarea desde el contexto
-  const { addTarea } = useContext(TareaContext);
+  // Accede a la función addTask desde el contexto
+  const { addTask } = useContext(TaskContext);
 
   // Función para manejar la adición de una nueva tarea
   const handleAdd = () => {
-    addTarea(text); // Llama a la función addTarea del contexto para agregar la tarea
+    addTask(text); // Llama a la función addTask del contexto para agregar la tarea
     setText(""); // Limpia el input tras agregar
   };
 
