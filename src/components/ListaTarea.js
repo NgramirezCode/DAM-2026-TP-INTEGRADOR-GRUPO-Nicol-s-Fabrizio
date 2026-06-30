238a5d3a3671a0ab1b0cc5390493fce3bb1d8dbc
Nicolas <ngramirez313an@gmail.com>
2026-06-30 16:13:37 -0300

    feat: Cambios en FlatList

--- src/components/ListaTarea.js
+++ src/components/ListaTarea.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { FlatList, View, Text } from "react-native";
+
 import Tarea from "./Tarea";
 
 // Componente ListaTarea que recibe una lista de tareas y un mensaje para mostrar cuando la lista está vacía
@@ -9,9 +10,8 @@ export default function ListaTarea({ data, emptyMessage }) {
     <FlatList
       data={data}
       keyExtractor={(item) => item.id}
-      renderItem={({ item }) => <Tarea Tarea={item} />}
+      renderItem={({ item }) => <Tarea task={item} />}
       ListEmptyComponent={<Text>{emptyMessage}</Text>}
-      contentContainerStyle={{ paddingBottom: 20 }}
     />
   );
 }
